ecf03a87ec20e988e0228a153f5379230ca0ee5b
Stephen Adeosun <stephenadeosun411@gmail.com>
2022-11-08 21:20:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function val(result){
+    form.disp.value = form.disp.value + result;
+  
+  }
+  function calculate(){
+      if(form.disp.value == ""){
+         alert("Please enter numbers");
+      }else{
+          form.disp.value =eval(form.disp.value );
+      }
+  }
+  var btn = form.veql;
+  btn.addEventListener('dblclick', function(){
+      form.disp.value = "";
+  });
